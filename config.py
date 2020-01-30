97527c5daaed699ad732e58a90e5e2b579dd9440
tristan-tsl <tanshilinmail@gmail.com>
2020-01-31 03:54:39 +0800

    添加初始化项目, 添加数据模型模块后端接口

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+import yaml
+
+import config
+
+project_root_path = os.getcwd()  # 项目根目录
+app_conf = {}
+
+
+def init():
+    with open(os.path.join(project_root_path, "configs", "application.yml")) as f:
+        config.app_conf = yaml.safe_load(f.read())
+    print("app_conf: ", app_conf)
