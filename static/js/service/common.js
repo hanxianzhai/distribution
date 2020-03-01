41c1c282b8e50d8a2ac4aeac4e7a19901d933c37
tristan-tsl <tanshilinmail@gmail.com>
2020-03-02 02:12:36 +0800

    重置项目

--- static/js/service/common.js
+++
@@ -1,6 +0,0 @@
-let url_prefix = "http://" + window.location.host;
-if (axios) {
-    axios.defaults.withCredentials = true;
-    axios.defaults.crossDomain = true;
-    axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
-}
