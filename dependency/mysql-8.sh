a9934af4ff55c5545c108e955dbebf0f7b72a0c9
tristan-tsl <tanshilinmail@gmail.com>
2020-03-08 10:52:49 +0800

    add the basic project dependency

---
+++ dependency/mysql-8.sh
@@ -0,0 +1,39 @@
+拉取docker镜像:
+docker pull mysql:8
+
+准备文件夹
+rm -rf /data/tristan/mysql/data
+mkdir -p /data/tristan/mysql/data
+
+准备binlog
+上传mysql.cnf到服务器
+mv -f mysql.cnf /data/tristan/mysql/mysql.cnf
+
+运行docker:
+docker stop mymysql
+docker rm   mymysql
+
+docker run --name  mymysql -p 3306:3306 --restart=always --privileged=true \
+    -v /data/tristan/mysql/mysql.cnf:/etc/mysql/conf.d/mysql.cnf \
+    -v /data/tristan/mysql/data:/var/lib/mysql  \
+    -e MYSQL_ROOT_PASSWORD=tristan123 -d mysql:8  \
+    --character-set-server=utf8mb4  \
+    --collation-server=utf8mb4_unicode_ci
+
+查看日志
+docker logs -f mymysql
+
+外部无法访问?
+
+# 进入容器内部:
+docker exec -it mymysql /bin/bash
+
+# 连接mysql
+mysql -uroot -p
+# 输入: tristan123
+
+# 修改访问设置
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'tristan123';
+
+# 刷新权限
+FLUSH PRIVILEGES;
