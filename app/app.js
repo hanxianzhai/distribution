474a10e63f4d8306d026a10f37cbe811459c6471
tristan-tsl <tanshilinmail@gmail.com>
2020-03-09 21:30:54 +0800

    init the distribution project need to padding

---
+++ app/app.js
@@ -0,0 +1,22 @@
+import '@babel/polyfill'
+import express from 'express'
+const bodyParser = require('body-parser');
+const { ApolloServer } = require('apollo-server-express');
+const cors = require('cors');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+const server = new ApolloServer({
+    modules: [
+
+    ],
+});
+
+server.applyMiddleware({ app });
+const version = require("../package.json").version;
+app.get('/', (req, res) => res.send(version));
+
+app.listen({ port: 5000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:5000`),
+);
---
+++ docs/dev.md
@@ -0,0 +1,9 @@
+https://blog.logrocket.com/from-rest-to-graphql/
+
+add the data model:
+1. design the database table and wirte to create
+2. npm install -g sequelize-auto mysql
+3. sequelize-auto -h localhost -d openlaas -u openlaas -x openlaas123 -p 3306  --dialect mysql -c sequelize-auto-settings.json -o app/models
+
+
+
