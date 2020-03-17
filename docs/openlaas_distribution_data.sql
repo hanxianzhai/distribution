ac12e86c5964ebb09c90f06ea04fbea8c364de3c
tristan-tsl <tanshilinmail@gmail.com>
2020-03-17 16:03:53 +0800

    revert the project

--- docs/openlaas_distribution_data.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `designer_data_directories`;
-CREATE TABLE `designer_data_directories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pid` int(11) DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `desc` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
