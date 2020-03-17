f09f9fb87c8a1c10e3fdd8ea010e06e913db4c49
tristan-tsl <tanshilinmail@gmail.com>
2020-03-17 22:02:08 +0800

    add the designer-data-struct

--- db/table_struct.sql
+++ db/table_struct.sql
@@ -5,3 +5,9 @@ CREATE TABLE `designer_data_directories` (
   `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `designer_data_struct` (
+  `id` int(11) NOT NULL,
+  `code` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
